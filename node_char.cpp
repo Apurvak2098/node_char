5feb6c33353584fbdb1762cc687b10700858039f
Apurvak2098 <76657360+Apurvak2098@users.noreply.github.com>
2021-01-15 21:49:02 +0530

    Add files via upload

---
+++ node_char.cpp
@@ -0,0 +1,94 @@
+// A recursive C++ program to print  
+// REVERSE level order traversal  
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+/* A binary tree node has data,  
+pointer to left and right child */
+class node  
+{  
+    public: 
+    char data;  
+    node* left;  
+    node* right;  
+};  
+  
+//Function protoypes
+void printGivenLevel(node* root, int level);  
+int height(node* node);  
+node* newNode(int data);  
+  
+/* Function to print REVERSE  
+level order traversal a tree*/
+void reverseLevelOrder(node* root)  
+{  
+    int h = height(root);  
+    int i;  
+    for (i=h; i>=1; i--) //THE ONLY LINE DIFFERENT FROM NORMAL LEVEL ORDER  
+        printGivenLevel(root, i);  
+}  
+  
+/* Print nodes at a given level */
+void printGivenLevel(node* root, int level)  
+{  
+    if (root == NULL)  
+        return;  
+    if (level == 1)  
+        cout << root->data << "\n";  
+    else if (level > 1)  
+    {  
+        printGivenLevel(root->right, level - 1);  
+        printGivenLevel(root->left, level - 1);  
+    }  
+}  
+  
+/* Compute the "height" of a tree -- the number of  
+    nodes along the longest path from the root node  
+    down to the farthest leaf node.*/
+int height(node* node)  
+{  
+    if (node == NULL)  
+        return 0;  
+    else
+    {  
+        /* compute the height of each subtree */
+        int lheight = height(node->left);  
+        int rheight = height(node->right);  
+  
+        /* use the larger one */
+        if (lheight > rheight)  
+            return(lheight + 1);  
+        else return(rheight + 1);  
+    }  
+}  
+  
+/* Helper function that allocates a new node with the  
+given data and NULL left and right pointers. */
+node* newNode(char data)  
+{  
+    node* Node = new node(); 
+    Node->data = data;  
+    Node->left = NULL;  
+    Node->right = NULL;  
+  
+    return(Node);  
+}  
+  
+/* Driver code*/
+int main()  
+{  
+    node *root = newNode('A');  
+    root->left = newNode('B');
+    root->left->left = newNode('C');
+    root->left->right = newNode('D');
+    root->right = newNode('E');
+    root->right->left = newNode('F');
+    root->right->right = newNode('G');
+    root->right->right->right = newNode('H');
+  
+    cout << "Level Order traversal of binary tree is \n";  
+    reverseLevelOrder(root);  
+  
+    return 0;  
+  
+}
